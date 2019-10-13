a6a7103af5027463092d76c88a74f823c8b547f2
YFdyh000 <yfdyh000@gmail.com>
2019-10-13 18:32:43 +0800

    Less building

--- src/VersionRev.h
+++ src/VersionRev.h
@@ -1,6 +1,6 @@
 #define BRANCH _T("zh-CN")
 #define VERSION_MINOR 19
 #define VERSION_BUILD 10
-#define VERSION_HASH TEXT("8abb500")
-#define VERSION_REV 2383
-#define VERSION_REV_FULL TEXT("r2383 (8abb500) (zh-CN) (master@2381)")
+#define VERSION_HASH TEXT("8f55f3b")
+#define VERSION_REV 2384
+#define VERSION_REV_FULL TEXT("r2384 (8f55f3b) (zh-CN) (master@2381)")
