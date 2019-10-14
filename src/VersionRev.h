9e9801f162685b70ca198786a2b844e6a7ff8990
YFdyh000 <yfdyh000@gmail.com>
2019-10-14 20:46:06 +0800

    updates

--- src/VersionRev.h
+++ src/VersionRev.h
@@ -1,6 +1,6 @@
 #define BRANCH _T("zh-CN")
 #define VERSION_MINOR 19
 #define VERSION_BUILD 10
-#define VERSION_HASH TEXT("8f55f3b")
-#define VERSION_REV 2384
-#define VERSION_REV_FULL TEXT("r2384 (8f55f3b) (zh-CN) (master@2381)")
+#define VERSION_HASH TEXT("0795a1f")
+#define VERSION_REV 2427
+#define VERSION_REV_FULL TEXT("r2427 (0795a1f) (zh-CN) (master@2381)")
